719498e59369e4d3aac8138229684f6630668490
M. Shelby Blanton <shelby@lionwoodvisual.com>
2016-09-08 20:45:29 -0700

    Final Proof Ready Commit.

--- content/classes/structure.php
+++ content/classes/structure.php
@@ -1,30 +1,32 @@
 <?php 
+date_default_timezone_set('America/Phoenix');
 
 class Structure {
 
-    private $header = '
+    public function header() {
+        $header = '
         <!DOCTYPE html>
         <html>
             <head>
                 <meta charset="UTF-8">
-                <title>Paseo Village - Phoenix, AZ - A Viewpoint Senior Care Community</title>
+                <title>Paseo Village - Peoria, AZ - A Viewpoint Senior Care Community</title>
+                <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Open+Sans|Ubuntu" rel="stylesheet">
                 <link rel="stylesheet" type="text/css" href="css/main.css">
             </head>
             <body>';
 
-    private $footer = '
-            <footer>This is the Footer.</footer>
-            <div class="copyright">&copy;<?=date("Y")?> Paseo Village. A Viewpoint Senior Care Community. All Rights Reserved.</div>
-            </body>
-        </html>';
-
-    public function header() {
-        return $this->header;
+        return $header;
     }
 
     public function footer() {
-        return $this->footer;
-    }
-}
+        $footer = '
+            <footer>Stay Tuned! Our website will be here soon!</footer>
+            <div class="copyright">&copy; '.date('Y').' Paseo Village. A Viewpoint Senior Care Community. All Rights Reserved.</div>
+            </body>
+        </html>';
 
+        return $footer;
+    }
+
+}
 ?>
--- index.php
+++ index.php
@@ -5,10 +5,21 @@ include 'content/classes/includes.php';
 echo $structure->header();
 
 ?>
+    <ul class="cb-slideshow">
+        <li><span>Img 1</span></li>
+        <li><span>Img 2</span></li>
+        <li><span>Img 3</span></li>
+        <li><span>Img 4</span></li>
+        <li><span>Img 5</span></li>
+        <li><span>Img 6</span></li>
+    </ul>
 
     <div class="white-box">
-        <h1>Paseo Village</h1>
-        <h2>A Viewpoint Senior Care Community</h2>
+        <div class="divider">
+            <img src="content/img/paseo-village-logo.png" alt="Paseo Village - A Viewpoint Senior Care Community - Peoria, AZ" width="300" height="210" />
+        </div>
+        <h1>Main <a href="tel:6238785115">(623) 878-5115</a> | Fax (623) 878-1604</h1>
+        <h2>10420 N 89th Ave, Peoria, AZ</h2>
     </div>
 
 <?php
